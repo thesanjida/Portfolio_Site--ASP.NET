07ddc53c47c11f1108c5256f3661b816a4d21a5a
Sanjida Alam <sanjida.alam.in.tech@gmail.com>
2023-09-21 02:22:01 +0600

    Add project files.

---
+++ Portfolio_Site/Controllers/PortfolioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio_Site.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Educational()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Project()
+        {
+            return View();
+        }
+    }
+}
